9d4f2df26d68731589ab32db44253f4d5bb8e4d9
SebastianRodCimas <56884033+SebastianRodCimas@users.noreply.github.com>
2021-05-18 11:37:00 +0200

    add ndk

--- lib/accueil/accueil.dart
+++ lib/accueil/accueil.dart
@@ -31,7 +31,7 @@ class Accueil extends StatelessWidget {
             ]))),
             ListTile(
               title: Text(
-                '                     Actualités',
+                '                     Documents',
                 style: TextStyle(
                   color: Color(0xFF951B81),
                   fontSize: 18,
